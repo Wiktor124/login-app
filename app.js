236a73901936d7571cce2066af71afcd3184d1b1
Wiktor <victortkd88@gmail.com>
2022-08-27 22:42:40 -0600

    new

--- app.js
+++ app.js
@@ -1,12 +1,12 @@
 const express = require('express');
 const app = express()
 
-const PORT = process.env.PORT || 3000 
+const PORT = process.env.PORT
 
 app.get('/', (req, res) => {
   res.send('la pagina ya funciona')
 })
 
-app.listen(PORT, (req, res) => {
+app.listen(PORT, () => {
   console.log('Server on port 3000')
 })
