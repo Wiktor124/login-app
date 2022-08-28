3be812d30b712dc408a90e9b8345af8a772ff420
Wiktor <victortkd88@gmail.com>
2022-08-27 22:53:45 -0600

    new properties

--- app.js
+++ app.js
@@ -3,10 +3,9 @@ const app = express()
 
 const PORT = process.env.PORT
 
-app.get('/', (req, res) => {
-  res.send('la pagina ya funciona')
-})
-
 app.listen(PORT, () => {
   console.log('Server on port 3000')
+})
+app.get('/', (req, res) => {
+  res.send('la pagina ya funciona')
 })
